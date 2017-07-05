9ea3d7bb06182473c49aba91f33808295517dec7
Lina Jones <lina.jones630@gmail.com>
2017-07-05 14:46:21 -0600

    working on routes

--- browser/react/components/Main.js
+++ browser/react/components/Main.js
@@ -4,6 +4,7 @@ import AllAlbums from './AllAlbums';
 import SingleAlbum from './SingleAlbum';
 import Sidebar from './Sidebar';
 import Player from './Player';
+import {HashRouter as Router, Route} from 'react-router-dom';
 
 export default class Main extends Component {
 
@@ -43,13 +44,12 @@ export default class Main extends Component {
         <div className="col-xs-2">
           <Sidebar deselectAlbum={this.deselectAlbum} />
         </div>
-        <div className="col-xs-10">
-        {
-          this.state.selectedAlbum.id ?
-          <SingleAlbum album={this.state.selectedAlbum} /> :
-          <AllAlbums albums={this.state.albums} selectAlbum={this.selectAlbum} />
-        }
-        </div>
+        <Router>
+          <div className="col-xs-10">
+            <Route exact path="/" component={AllAlbums} />
+            <Route path="/albums" component={AllAlbums} />
+          </div>
+        </Router>
         <Player />
       </div>
     );
